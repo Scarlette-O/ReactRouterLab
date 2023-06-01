6339b81fcc7661c38def2dae6c5ef5e8dd305677
Harshil Rama <harshilr98@gmail.com>
2023-06-01 18:55:18 -0500

    Red and Blue

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const Red = (props) => {
   return (
@@ -19,16 +20,27 @@ const Blue = (props) => {
 
 const Main = (props) => {
   return (
-    <div id='container'>
 
-      <div id='navbar'>
-        {/* navigation here */}
-      </div>
+    <BrowserRouter>
+      <div id='container'>
 
-      <div id='main-section'>
-        {/* routes here */}
+
+        <div id='navbar'>
+          {/* navigation here */}
+        </div>
+
+        <div id='main-section'>
+          <Route path="/red">
+            <Red />
+          </Route>
+
+          <Route path="/blue">
+            <Blue />
+          </Route>
+
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
